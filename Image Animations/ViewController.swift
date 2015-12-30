037d3e8365d76e06ce4002161f8fff9ada289601
Krish Furia <krishfuria13@users.noreply.github.com>
2015-12-29 23:51:00 -0800

    Initial Commit

---
+++ Image Animations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Image Animations
+//
+//  Created by Krish Furia on 12/29/15.
+//  Copyright © 2015 Krish Furia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
